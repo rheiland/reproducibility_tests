8229e447464e177499f657b2f2d7c19eb1d3f607
Randy Heiland <randy.heiland@gmail.com>
2023-06-18 07:00:47 -0400

    update

--- reprod_test.sh
+++ reprod_test.sh
@@ -8,31 +8,41 @@ make
 python params_run.py heterogeneity params_heterog.txt
 make reset
 make template
+make
 python params_run.py project params_template.txt
 make reset
 make biorobots-sample
+make
 python params_run.py biorobots params_biorobots.txt
 make reset
 make cancer-biorobots-sample
+make
 python params_run.py project params_cancer_biorobots.txt   # bug? exec is "project"
 make reset
 make celltypes3-sample
+make
 python params_run.py celltypes3 params_celltypes3.txt
 make reset
 make pred-prey-farmer
+make
 python params_run.py pred_prey params_predprey.txt
 make reset
 make virus-macrophage-sample
+make
 python params_run.py virus-sample params_virus_mac.txt
 make reset
 make worm-sample
+make
 python params_run.py worm params_worm.txt
 make reset
 make interaction-sample
+make
 python params_run.py interaction_demo params_worm.txt
 make reset
 make mechano-sample
+make
 python params_run.py project params_worm.txt   # bug? "project"
 make reset
 make rules-sample
+make
 python params_run.py project params_rules.txt   # bug? "project"
